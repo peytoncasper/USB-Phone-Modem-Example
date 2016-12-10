f5b4e0b5e75a25a0c2beac2ea1625078bb8f19d9
Peyton Casper <peytoncas@gmail.com>
2016-12-10 14:08:19 -0600

    Initial Commit

---
+++ USB-Phone-Modem-Example/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serialDevices = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialDevices
+            // 
+            this.serialDevices.FormattingEnabled = true;
+            this.serialDevices.Location = new System.Drawing.Point(12, 12);
+            this.serialDevices.Name = "serialDevices";
+            this.serialDevices.Size = new System.Drawing.Size(523, 160);
+            this.serialDevices.TabIndex = 0;
+            this.serialDevices.SelectedValueChanged += new System.EventHandler(this.serialDevices_SelectedValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(523, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Send Command";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 207);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 68);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "AT Command";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 315);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.serialDevices);
+            this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox serialDevices;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ USB-Phone-Modem-Example/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Main : Form
+    {
+        SerialPort phonePort = new SerialPort();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            var serialPortList = SerialPort.GetPortNames();
+            foreach(var serialPort in serialPortList)
+            {
+                serialDevices.Items.Add(serialPort);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialDevices.Items.Clear();
+            var serialPortList = SerialPort.GetPortNames();
+            foreach (var serialPort in serialPortList)
+            {
+                serialDevices.Items.Add(serialPort);
+            }
+        }
+
+        private void serialDevices_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var device = serialDevices.SelectedItem;
+            if (device != null)
+            {
+                try
+                {
+                    phonePort.PortName = device.ToString();
+                    phonePort.BaudRate = 9600;
+                    phonePort.DataBits = 8;
+                    phonePort.StopBits = StopBits.One;
+                    phonePort.Parity = Parity.None;
+                    phonePort.ReadTimeout = 1000;
+                    phonePort.WriteTimeout = 1000;
+                    phonePort.Encoding = Encoding.GetEncoding("iso-8859-1");
+                    phonePort.DataReceived += new SerialDataReceivedEventHandler(serialReplyRecieved);
+                    phonePort.Open();
+                    phonePort.DtrEnable = true;
+                    phonePort.RtsEnable = true;
+                    MessageBox.Show("Connected");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error opening port: " + ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Select a device");
+        }
+
+        private void serialReplyRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            MessageBox.Show(phonePort.ReadLine());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (phonePort.IsOpen)
+                phonePort.WriteLine("AT+CREG=2");
+            else
+                MessageBox.Show("Select a device first");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
